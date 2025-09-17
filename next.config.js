2dffd6a89536c79b2c83926ba2d517ea6538b32e
tolik <tol.kowalev@gmail.com>
2025-09-17 16:47:44 +0300

    new publish

--- next.config.js
+++
@@ -1,8 +0,0 @@
-// next.config.js
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  output: "export", // ensures next export works
-  basePath: isProd ? "/lake-map" : "",
-  assetPrefix: isProd ? "/lake-map/" : "",
-};
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export", // ✅ correct
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? "/lake-map" : "",
+  assetPrefix: isProd ? "/lake-map/" : "",
 };
 
 export default nextConfig;
